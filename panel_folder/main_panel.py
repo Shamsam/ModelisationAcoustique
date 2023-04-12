751e0b59ec0dd0bfed8fb736c4bd9c97f872e50c
Shamsam <60717097+Shamsam@users.noreply.github.com>
2023-04-11 20:06:56 -0400

    co

--- panel_folder/main_panel.py
+++ panel_folder/main_panel.py
@@ -1,69 +1,59 @@
-import numpy as np
 import pyroomacoustics as pra
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 import panel as pn
 import param
 import holoviews as hv
+import numpy as np
 from response_calculations import freq_resp, compute_rir, calculate_responses
 
 pn.extension()
+hv.extension("bokeh")
 
-input_dim = pn.widgets.LiteralInput(name="Room dimensions", value=[5, 4, 3])
-input_absorption = pn.widgets.FloatSlider(name="Absorption coefficient", start=0, end=1, value=0.5, step=0.1)
+class RoomResponse(param.Parameterized):
+    room_dim = param.List(default=[5, 4, 3])
+    absorption = param.Number(default=0.5, bounds=(0, 1))
+    max_order = param.Integer(default=3, bounds=(0, None))
 
-input_mic_pos = pn.widgets.LiteralInput(name="Mic position", value=[2.5, 2, 1])
+    def __init__(self, **params):
+        super().__init__(**params)
+        self.mic_positions = {
+            "mic_1": [2.5, 2, 1],
+            "mic_2": [2.5, 2.5, 1],
+            "mic_3": [2.5, 3, 1]
+        }
 
-input_src_pos = pn.widgets.LiteralInput(name="Source position", value=[1, 1, 1.5])
+        self.src_positions = {
+            "src_1": [1, 1, 1.5],
+            "src_2": [3, 1, 1.5],
+            "src_3": [2, 1, 1.5]
+        }
 
-class Room(param.Parameterized):
-    room_dim = param.List(default=[5, 5, 5])
-    absorption = param.Number(default=0.5)
+        self.room = compute_rir(self.room_dim, self.absorption, self.max_order, self.mic_positions, self.src_positions)
+        self.freq_responses = calculate_responses(self.room, self.mic_positions, self.src_positions)
 
-    @param.depends('room_dim', watch=True)
-    def update_room_dim(self):
-        self.room_dim = input_dim.value
+    @param.depends("room_dim", "absorption", "max_order", watch=True)
+    def update_responses(self):
+        self.room = compute_rir(self.room_dim, self.absorption, self.max_order, self.mic_positions, self.src_positions)
+        self.freq_responses = calculate_responses(self.room, self.mic_positions, self.src_positions)
 
-    @param.depends('absorption', watch=True)
-    def update_absorption(self):
-        self.absorption = input_absorption.value
+    @param.depends("room_dim", "absorption", "max_order")
+    def plot_responses(self):
+        hv_plot = hv.Overlay()
 
+        for mic_idx in range(len(self.mic_positions)):
+            freq, response = self.freq_responses[f"mic_{mic_idx + 1}"]
+            response_db = 20 * np.log10(np.abs(response))
+            hv_plot *= hv.Curve((freq, response_db), label=f"mic_{mic_idx + 1}")
 
-class Mic(param.Parameterized):
-    mic_pos = param.List(default=[1, 1, 1])
-
-    @param.depends('mic_pos', watch=True)
-    def update_mic_pos(self):
-        self.mic_pos = input_mic_pos.value
-
-class Src(param.Parameterized):
-    src_pos = param.List(default=[1, 1, 1])
-
-    @param.depends('src_pos', watch=True)
-    def update_src_pos(self):
-        self.src_pos = input_src_pos.value
-
-room = Room()
-print(room)
-
-""" mic_positions = {
-    "mic_1": [2.5, 2, 1],
-    "mic_2": [2.5, 2.5, 1],
-    "mic_3": [2.5, 3, 1]
-}
-
-src_positions = {
-    "src_1": [1, 1, 1.5],
-    "src_2": [3, 1, 1.5],
-    "src_3": [2, 1, 1.5]
-}
+        hv_plot.opts(xlabel="Frequency (Hz)", ylabel="Response (dB)", title="Frequency Response", width=800, height=500)
+        return hv_plot
 
 
+room_response = RoomResponse()
+params_panel = pn.Param(room_response, parameters=["room_dim", "absorption", "max_order"], show_name=False, width=300)
+response_plot = pn.panel(room_response.plot_responses)
 
-room = compute_rir(room_dim, absorption, 3, mic_positions, src_positions)
+pn.Row(params_panel, response_plot).servable()
 
-freq_responses = calculate_responses(room, mic_positions, src_positions) """
-
-
-
-
+# panel serve --show --autoreload panel_folder/main_panel.py
---
+++ panel_folder/old_main.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pyroomacoustics as pra
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+import panel as pn
+import param
+import holoviews as hv
+from response_calculations import freq_resp, compute_rir, calculate_responses
+
+pn.extension()
+
+input_dim = pn.widgets.LiteralInput(name="Room dimensions", value=[5, 4, 3])
+input_absorption = pn.widgets.FloatSlider(name="Absorption coefficient", start=0, end=1, value=0.5, step=0.1)
+
+input_mic_pos = pn.widgets.LiteralInput(name="Mic position", value=[2.5, 2, 1])
+
+input_src_pos = pn.widgets.LiteralInput(name="Source position", value=[1, 1, 1.5])
+
+class Room(param.Parameterized):
+    room_dim = param.List(default=[5, 5, 5])
+    absorption = param.Number(default=0.5)
+
+    @param.depends('room_dim', watch=True)
+    def update_room_dim(self):
+        self.room_dim = input_dim.value
+
+    @param.depends('absorption', watch=True)
+    def update_absorption(self):
+        self.absorption = input_absorption.value
+
+
+class Mic(param.Parameterized):
+    mic_pos = param.List(default=[1, 1, 1])
+
+    @param.depends('mic_pos', watch=True)
+    def update_mic_pos(self):
+        self.mic_pos = input_mic_pos.value
+
+class Src(param.Parameterized):
+    src_pos = param.List(default=[1, 1, 1])
+
+    @param.depends('src_pos', watch=True)
+    def update_src_pos(self):
+        self.src_pos = input_src_pos.value
+
+room = Room()
+print(room)
+
+mic_positions = {
+    "mic_1": [2.5, 2, 1],
+    "mic_2": [2.5, 2.5, 1],
+    "mic_3": [2.5, 3, 1]
+}
+
+src_positions = {
+    "src_1": [1, 1, 1.5],
+    "src_2": [3, 1, 1.5],
+    "src_3": [2, 1, 1.5]
+}
+
+
+
+room = compute_rir(room_dim, absorption, 3, mic_positions, src_positions)
+
+freq_responses = calculate_responses(room, mic_positions, src_positions)
+
+# panel serve --show --autoreload panel_folder/testing_app.py
